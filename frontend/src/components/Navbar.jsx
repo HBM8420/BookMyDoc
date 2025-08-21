0b15eb46cbd4a55e9121f7d3a6c906e3a3604a39
HBM8420 <harshmishra8420@gmail.com>
2025-08-22 00:28:27 +0530

    Logo Changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img
         className="w-44 cursor-pointer"
-        src={assets.logo}
+        src={assets.logo2}
         alt="Logo"
         onClick={() => navigate("/")}
       />
